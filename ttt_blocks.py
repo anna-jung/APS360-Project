6dfff6df5bebe611463be2bbf163688c74fa8746
Kevin-Ni22 <kevin.ni@mail.utoronto.ca>
2025-08-02 17:02:32 -0400

    Added TTT self supervised module - renamed ttt_mlp -> ttt_blocks

--- ttt_blocks.py
+++ ttt_blocks.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from stable_audio_tools import get_pretrained_model
 from stable_audio_tools.inference.generation import generate_diffusion_cond
 from stable_audio_tools.data.utils import read_video, merge_video_audio
@@ -22,3 +24,19 @@ def ttt_prime(ttt, x, dim=1):
     x_rev = torch.flip(x, dims=[dim])
     ttt_rev = ttt(x_rev)
     return torch.flip(ttt_rev, dims=[dim])
+
+# Define TTT self-supervised module
+class ttt_self_supervised_module(nn.Module):
+    def __init__(self, input_dim, proj_dim, ttt_module_cls):
+        super().__init__()
+        self.theta_K = nn.Linear(input_dim, proj_dim, bias=False)
+        self.theta_V = nn.Linear(input_dim, proj_dim, bias=False)
+        self.ttt_module = ttt_module_cls(proj_dim)
+        self.proj_dim = proj_dim
+
+    def forward(self, x):
+        x_k = self.theta_K(x)
+        x_v = self.theta_V(x)
+        out = self.ttt_module(x_k)
+        loss = F.mse_loss(out, x_v)
+        return loss
