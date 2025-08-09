e39992854490df4036bcdd227cb672804487bc0e
Kevin-Ni22 <kevin.ni@mail.utoronto.ca>
2025-08-09 12:54:18 -0400

    Added train.py, includes inner loop.

--- train.py
+++ train.py
@@ -8,24 +8,29 @@ from stable_audio_tools.inference.generation import generate_diffusion_cond
 from stable_audio_tools.data.utils import load_and_process_audio
 
 
-def run_ttt_inner_loop(ttt_model, x_latent_btd, num_iterations, lr=0.01):
+def run_ttt_inner_loop(x_latent_btd, num_iterations, ss=ttt_self_supervised_module, lr=0.01, use_ttt_prime=False):
 
     # Freeze the parameters of the outer loop
-    for p in ttt_model.theta_K.parameters(): p.requires_grad_(False)
-    for p in ttt_model.theta_V.parameters(): p.requires_grad_(False)
+    for p in ss.theta_K.parameters(): p.requires_grad_(False)
+    for p in ss.theta_V.parameters(): p.requires_grad_(False)
 
-    inner_params = list(ttt_model.ttt_module.parameters())
+    inner_params = list(ttt_self_supervised_module.ttt_module.parameters())
     optimizer = torch.optim.Adam(inner_params, lr=lr)
     
-    ttt_model
+    ss.train()
+
     for i in range(num_iterations):
         optimizer.zero_grad()
-        xk = ttt_model.theta_K(x_latent_btd)
-        xv = ttt_model.theta_V(x_latent_btd)
-        xk_rev = torch.flip(xk, dims=[1])
-        out = ttt_model.ttt_module(xk_rev)
-        out = torch.flip(out, dims=[1])
-        loss = F.mse_loss(out, xv)
+        if use_ttt_prime:
+            xk = ss.theta_K(x_latent_btd)
+            xv = ss.theta_V(x_latent_btd)
+            xk_rev = torch.flip(xk, dims=[1])
+            out = ss.ttt_module(xk_rev)
+            out = torch.flip(out, dims=[1])
+            loss = F.mse_loss(out, xv)
+        else:
+            loss = ss(x_latent_btd)
+
         loss.backward()
         optimizer.step()
-    return ttt_model.ttt_module
+    return ss.ttt_module
